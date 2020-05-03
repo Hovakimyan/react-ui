957a001889ba0f57479d1a627e94a86f870b6aa5
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-03 18:11:38 +0400

    feat(src): update babel configs

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,6 @@ const { NODE_ENV } = process.env
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
     plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-transform-flow-strip-types',
-        'transform-es2015-modules-commonjs',
-        'babel-plugin-dynamic-import-node',
-        'babel-plugin-styled-components',
-        'inline-react-svg',
         [
             'module-resolver',
             {
