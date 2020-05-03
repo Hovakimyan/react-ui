a512359415d6323333c6bfd7a447fdba6604b357
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-03 17:53:26 +0400

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+rm -rf ./lib
+
+node ./scripts/update-version.js
+
+git add ./package.json
+
+git commit -m "update version"
+
+git push
+
+./node_modules/.bin/babel ./src --out-dir ./lib --ignore '**/stories.js','**/types.js','**/*.markdown.md'
+
+rm -rf ./lib/flow-typed
+
+cp ./package.json ./lib
+
 cp npm-readme.md ./lib/README.md
-#rm -rf ./lib
-#
-#node ./scripts/update-version.js
-#
-#git add ./package.json
-#
-#git commit -m "update version"
-#
-#git push
-#
-#./node_modules/.bin/babel ./src --out-dir ./lib --ignore '**/stories.js','**/types.js','**/*.markdown.md'
-#
-#rm -rf ./lib/flow-typed
-#
-#cp ./package.json ./lib
-#
-#cp npm-readme.md ./README.md
-#
-#npm publish ./lib
+
+npm publish ./lib
