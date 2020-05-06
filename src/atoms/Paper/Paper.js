826c6a624aa102f55450030f20edbf1036e1ee33
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-05-06 22:51:03 +0400

    fix: udpate paper content

--- src/atoms/Paper/Paper.js
+++ src/atoms/Paper/Paper.js
@@ -4,13 +4,15 @@ import React from 'react'
 import * as Styled from './styled'
 import type { Props } from './types'
 
-const Paper = (props: Props) => {
-    const { corner = 'rounded', shadow = 'shallow', ...rest } = props
-    return (
-        <Styled.Paper {...rest} corner={corner} shadow={shadow}>
-            {props.children}
-        </Styled.Paper>
-    )
-}
+const Paper = ({
+    corner = 'rounded',
+    shadow = 'shallow',
+    children,
+    ...rest
+}: Props) => (
+    <Styled.Paper {...rest} corner={corner} shadow={shadow}>
+        {children}
+    </Styled.Paper>
+)
 
 export default Paper
