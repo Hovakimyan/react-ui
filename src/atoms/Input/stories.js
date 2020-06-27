d5de0e050aaf118c92cbe27a3766f91cd499e957
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-06-27 15:52:41 +0400

    feat(src): update stories

--- src/atoms/Input/stories.js
+++ src/atoms/Input/stories.js
@@ -1,41 +1,21 @@
 import React, { useState } from 'react'
 import { Input } from './index'
-import Theme from 'theme'
-import { storiesOf } from '@storybook/react'
-import { boolean, select, withKnobs } from '@storybook/addon-knobs'
-import { jsxDecorator } from 'storybook-addon-jsx'
+import { boolean, select } from '@storybook/addon-knobs'
 import { buttonSizeOptions } from 'consts/options'
 
-const stories = storiesOf('Atoms|Input', module)
-
-stories.addDecorator(withKnobs)
-stories.addDecorator(jsxDecorator)
-
-stories.add('Default', () => {
+export const Sample = () => {
     const [value, setValue] = useState('')
     return (
-        <Theme>
-            <div
-                style={{
-                    padding: '20px',
-                    boxSizing: 'border-box',
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}
-            >
-                <Input
-                    // required
-                    onChange={(e) => setValue(e.target.value)}
-                    value={value}
-                    placeholder="write text"
-                    size={select('Size', buttonSizeOptions, 'medium')}
-                    // optional
-                    isFit={boolean('Is Fit', false)}
-                    isFlat={boolean('Is Flat', false)}
-                    disabled={boolean('Disabled', false)}
-                />
-            </div>
-        </Theme>
+        <Input
+            // required
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+            placeholder="write text"
+            size={select('Size', buttonSizeOptions, 'medium')}
+            // optional
+            isFit={boolean('Is Fit', false)}
+            isFlat={boolean('Is Flat', false)}
+            disabled={boolean('Disabled', false)}
+        />
     )
-})
+}
--- src/atoms/Loading/stories.js
+++ src/atoms/Loading/stories.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import { Loading } from './index'
-import Theme from 'theme'
-import { storiesOf } from '@storybook/react'
-import { select, withKnobs } from '@storybook/addon-knobs'
-import { jsxDecorator } from 'storybook-addon-jsx'
+import { select } from '@storybook/addon-knobs'
 import { buttonSizeOptions, colorOptions } from '../../consts/options'
 
-const stories = storiesOf('Atoms|Loading', module)
-
-stories.addDecorator(withKnobs)
-stories.addDecorator(jsxDecorator)
-
-stories.add('Default', () => (
-    <Theme>
-        <Loading
-            size={select('Size', buttonSizeOptions, 'medium')}
-            color={select('Color', colorOptions, 'secondaryExtraDark')}
-        />
-    </Theme>
-))
+export const Sample = () => (
+    <Loading
+        size={select('Size', buttonSizeOptions, 'medium')}
+        color={select('Color', colorOptions, 'secondaryExtraDark')}
+    />
+)
