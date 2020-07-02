f9349ff4997ccfba3e960d5e94ab3ef5f6c3d223
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-07-02 22:43:13 +0400

    feat(src): start migration to typescript

---
+++ src/global.d.ts
@@ -0,0 +1,51 @@
+type ThemeColorTypes = 'primary' | 'transparent'
+
+type SizeTypes = 'small' | 'medium' | 'large'
+
+type FontSizeType =
+    | 'xxSmall'
+    | 'xSmall'
+    | 'small'
+    | 'medium'
+    | 'large'
+    | 'xLarge'
+    | 'xxLarge'
+    | number
+
+type IconSizeType =
+    | 'xxSmall'
+    | 'xSmall'
+    | 'small'
+    | 'medium'
+    | 'large'
+    | 'xLarge'
+    | 'xxLarge'
+    | number
+
+type GlobalColorType =
+    | 'white'
+    | 'black'
+    | 'secondaryExtraDark'
+    | 'secondaryDark'
+    | 'secondarySemiDark'
+    | 'secondary'
+    | 'secondarySemiLight'
+    | 'secondaryLight'
+    | 'secondaryExtraLight'
+    | 'secondaryUltraLight'
+    | 'primaryExtraDark'
+    | 'primaryDark'
+    | 'primarySemiDark'
+    | 'primary'
+    | 'primarySemiLight'
+    | 'primaryLight'
+    | 'primaryExtraLight'
+    | 'green'
+    | 'purple'
+    | 'yellow'
+    | 'coral'
+    | 'cyan'
+    | 'red'
+
+type OptionValueType = string | number
+type OptionType = { label: OptionValueType, value: OptionValueType }
